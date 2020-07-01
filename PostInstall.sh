3a3080740d1c14aa6fd94ff9c2bfc110523161c2
distro-1972 <52358711+distro-1972@users.noreply.github.com>
2020-06-30 21:29:36 -0600

    Delete PostInstall.sh

--- PostInstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-arch-chroot /mnt useradd -mU -s /usr/bin/zsh -G wheel,uucp,video,audio,storage,games,input "$user"
-arch-chroot /mnt chsh -s /usr/bin/zsh
-
-echo "$user:$password" | chpasswd --root /mnt
-echo "root:$password" | chpasswd --root /mnt
